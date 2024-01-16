32bb372e0417d9496a206153b5696982326d7328
Quentin <quentin.moessner@bellman.immo>
2024-01-16 12:01:51 +0100

    chore: remove reference to prisma

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,4 @@
 import type { Handle } from '@sveltejs/kit';
-import { prisma } from '$lib/server/prisma';
-
 export const handle = (async ({ event, resolve }) => {
-	event.locals.prisma = prisma;
 	return resolve(event);
 }) satisfies Handle;
