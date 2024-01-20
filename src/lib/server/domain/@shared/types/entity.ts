8c7813e75d98a3396091338648f05827c90bda4f
Quentin Moessner <quentin.moessner@gmail.com>
2024-01-20 12:26:26 +0100

    feat(mvp): ensure any entity is branded (#6)

--- src/lib/server/domain/@shared/types/entity.ts
+++ src/lib/server/domain/@shared/types/entity.ts
@@ -1,5 +1,6 @@
-export interface BaseEntity {
-	id: string;
-	createdAt: Date;
-	updatedAt: Date;
-}
+import type { Branded } from '$brand';
+
+export type Entity<ID extends Branded<unknown, string>, T> = { id: ID } & T & {
+		createdAt: Date;
+		updatedAt: Date;
+	};
