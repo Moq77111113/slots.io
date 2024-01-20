cadb4d6fae2aed7961a4bf85fe12e719ac980553
Quentin Moessner <quentin.moessner@gmail.com>
2024-01-20 12:31:02 +0100

    feat(mvp): brand events et slots

--- src/lib/server/domain/event/attributes/availability.ts
+++ src/lib/server/domain/event/attributes/availability.ts
@@ -4,6 +4,6 @@ export type AvailabilityStatus = 'available' | 'unavailable' | 'maybe';
  * Represents the availability of a user for a specific slot.
  */
 export type Availability = {
-	userId: string;
+	userId: unknown; //TODO replace by userId;
 	status: AvailabilityStatus;
 };
--- src/lib/server/domain/event/models/event.ts
+++ src/lib/server/domain/event/models/event.ts
@@ -1,15 +1,20 @@
 import type { Entity } from '$domain/@shared';
+import type { Branded } from '$brand';
 import type { Slot } from './slot';
 
+export type EventId = Branded<string, 'EventId'>;
 /**
  * @description Aggregate root entity representing an event with associated slots.
  */
-export type Event = Entity<{
-	title: string;
-	description?: string;
-	creator: unknown; // TODO replace by user;
-	slots: Slot[];
-	locked: boolean;
-	expiration?: Date;
-	participants: unknown[]; // TODO replace by user;
-}>;
+export type Event = Entity<
+	EventId,
+	{
+		title: string;
+		description?: string;
+		creatorId: unknown; // TODO replace by userId;
+		slots: Slot[];
+		locked: boolean;
+		expiration?: Date;
+		participantIds: unknown[]; // TODO replace by userId;
+	}
+>;
