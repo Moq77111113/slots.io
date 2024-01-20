8c69121a48dc0e0c4bfb78cf265a7fd5ebefecb5
Quentin Moessner <quentin.moessner@gmail.com>
2024-01-20 15:58:18 +0100

    feat(users): add status attribute

--- src/lib/server/domain/user/models/user.ts
+++ src/lib/server/domain/user/models/user.ts
@@ -1,13 +1,13 @@
 import { make, type Branded } from '$brand';
 import type { Entity, Locale, Language } from '$domain/@shared';
 import type { ThirdPartyAccount } from '$domain/@shared/attributes';
-import type { NotificationChannel } from '../attributes/notification-channel';
+import type { UserStatus, NotificationChannel } from '$domain/user/attributes';
 
 export type UserId = Branded<string, 'UserId'>;
 
 const assertsUserId = (id: string): asserts id is UserId => {
 	if (typeof id !== 'string') throw new Error('Invalid person id');
-}
+};
 
 export const makeUserId = (id: string) => make(id, assertsUserId);
 
@@ -26,5 +26,6 @@ export type User = Entity<
 		locale: Locale;
 		password?: string;
 		accessToken?: string;
+		status: UserStatus;
 	}
 >;
