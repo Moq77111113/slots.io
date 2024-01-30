2d581bf639ed5ae8edee4d099733a82b086bf363
Quentin Moessner <quentin.moessner@gmail.com>
2024-01-30 18:21:03 +0100

    feat(auth): third party request api

---
+++ src/lib/server/application/actions/user/thirdPartyRequest.action.ts
@@ -0,0 +1,20 @@
+import type { ThirdPartyApi } from '$domain/user/ports/api/user.api';
+
+type LogoutUserActionContext = {
+	services: {
+		user: ThirdPartyApi;
+	};
+};
+
+export const GenerateOAuthRequest = (context: LogoutUserActionContext) => {
+	const { services } = context;
+	const _name = 'user.logout' as const;
+
+	const execute = async (provider: string) => {
+		return await services.user.generateThirdPartyRequest(provider);
+	};
+
+	return {
+		execute
+	};
+};
