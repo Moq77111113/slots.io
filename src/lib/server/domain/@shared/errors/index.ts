60f6e97852389d5608a69dbbe4611eaef73b3ef0
Quentin Moessner <quentin.moessner@gmail.com>
2024-02-01 18:24:47 +0100

    types: sanitize error generics

--- src/lib/server/domain/@shared/errors/index.ts
+++ src/lib/server/domain/@shared/errors/index.ts
@@ -1,17 +1,19 @@
 import type { DomainError, ErrorCollection } from '$domain/@shared/errors/errors';
+import { type PollErrorKeys, PollErrors } from '$domain/@shared/errors/poll.errors';
 import type { ErrorHandler } from '$domain/@shared/errors/ports/spi/error.handler';
 import { type UserErrorKeys, UserErrors } from '$domain/@shared/errors/user.errors';
 
 /**
  * @description List of keys available for domain errors
  */
-type DomainErrorKeys = UserErrorKeys;
+type DomainErrorKeys = UserErrorKeys | PollErrorKeys;
 
 /**
  * @description Collection of domain errors
  */
 const DomainErrors = {
-	User: UserErrors
+	User: UserErrors,
+	Poll: PollErrors
 } as const satisfies Record<string, ErrorCollection<DomainErrorKeys>>;
 
 export { DomainErrors };
