ccf0d5f77814a42fe8117251f7820113a100bec6
Quentin Moessner <quentin.moessner@gmail.com>
2024-02-01 18:25:12 +0100

    feat(poll): poll dto

--- src/lib/server/domain/poll/dto/in/poll-input.ts
+++ src/lib/server/domain/poll/dto/in/poll-input.ts
@@ -1,4 +1,9 @@
 import type { CreateEntityInput } from '$domain/@shared/types';
 import type { Poll } from '$domain/poll/models';
 
-export type CreatePollDto = Omit<CreateEntityInput<Poll>, 'creatorId' | 'participantIds'>;
+import type { CreateSlotDto } from './slot-input';
+
+export type CreatePollDto = Omit<
+	CreateEntityInput<Poll>,
+	'creatorId' | 'participantIds' | 'locked' | 'slots'
+> & { slots: CreateSlotDto[] };
