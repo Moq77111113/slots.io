8f2eeca90c58cb4eec9fae5de621297bbcb219ca
Quentin Moessner <quentin.moessner@gmail.com>
2024-02-01 18:26:52 +0100

    feat(poll): add api port

--- src/lib/server/domain/poll/models/slot.ts
+++ src/lib/server/domain/poll/models/slot.ts
@@ -1,8 +1,15 @@
 import type { Entity } from '$domain/@shared';
-import type { Branded } from '$domain/@shared/utils/brand';
+import { type Branded, make } from '$domain/@shared/utils/brand';
 import type { Availability } from '$domain/poll/attributes';
 
 export type SlotId = Branded<string, 'SlotId'>;
+
+const assertSlotId = (id: string): asserts id is SlotId => {
+	if (typeof id !== 'string') throw new Error('Invalid Poll id');
+};
+
+export const makeSlotId = (id: string) => make(id, assertSlotId);
+
 /**
  * Represents a time slot associated with a poll.
  */
