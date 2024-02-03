6423d2dbbb45ce494fa5c3bc5fb9c5c030b56d85
Quentin Moessner <quentin.moessner@gmail.com>
2024-02-03 11:51:27 +0100

    refactor(infrastructure): more error provider to infrastructure

--- src/lib/server/infrastructure/providers/error/default.error.provider.ts
+++ src/lib/server/infrastructure/providers/error/default.error.provider.ts
@@ -1,6 +1,6 @@
 import type { DomainError, ErrorHandler } from '$domain/@shared/errors';
 
-type DefaultErrorHandlerContext = {
+type DefaultErrorProviderContext = {
 	//
 };
 
@@ -8,15 +8,15 @@ type DefaultErrorHandlerContext = {
  *
  * @description This is the default error handler. It will log the error and throw it, we'll change it later
  */
-export const DefaultErrorHandler = (_context: DefaultErrorHandlerContext): ErrorHandler => {
+export const DefaultErrorProvider = (_context: DefaultErrorProviderContext): ErrorHandler => {
 	const throws = (error: DomainError) => {
 		const date = new Date();
 		const hour = date.getHours();
 		const minutes = date.getMinutes();
 		const seconds = date.getSeconds();
 		const milliseconds = date.getMilliseconds();
-		const timelog = `${hour}:${minutes}:${seconds}:${milliseconds}`;
-		console.error(`${timelog}\t ${error.key} : ${error.message}`);
+		const timeLog = `${hour}:${minutes}:${seconds}:${milliseconds}`;
+		console.error(`${timeLog}\t ${error.key} : ${error.message}`);
 		throw new Error(error.message);
 	};
 	return { throws };
---
+++ src/lib/server/infrastructure/providers/index.ts
@@ -0,0 +1,2 @@
+export * from './auth/sb.auth.provider';
+export * from './error/default.error.provider';
