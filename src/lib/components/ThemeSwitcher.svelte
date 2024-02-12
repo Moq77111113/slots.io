efc274451ba35fe5dbbb7663da786284e02320ca
Quentin Moessner <quentin.moessner@gmail.com>
2024-02-12 09:10:22 +0100

    ✨ feat(ui): resp dialog component (#39)

--- src/lib/components/ThemeSwitcher.svelte
+++ src/lib/components/ThemeSwitcher.svelte
@@ -6,7 +6,7 @@
 	const theme = createLocalStorageTheme();
 </script>
 
-<Button on:click={theme.toggle} variant="outline" size="icon">
+<Button on:click={theme.toggle} variant="ghost" size="icon">
 	<Sun
 		class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
 	/>
